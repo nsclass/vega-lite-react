dfa12cb154e8a723e5a57e980d799ce767d7f4e4
Nam Seob Seo <nsclass@gmail.com>
2021-06-17 07:49:21 -0500

    updated readme file

--- src/App.js
+++ src/App.js
@@ -5,18 +5,6 @@ import * as vega from "vega"
 import * as vegaLite from "vega-lite"
 import { csv } from "d3"
 
-const data = [
-  { a: "A", b: 28 },
-  { a: "B", b: 55 },
-  { a: "C", b: 43 },
-  { a: "D", b: 91 },
-  { a: "E", b: 81 },
-  { a: "F", b: 53 },
-  { a: "G", b: 19 },
-  { a: "H", b: 87 },
-  { a: "I", b: 52 },
-]
-
 async function getData(url) {
   const data = await csv(url)
   console.log(data[0])
