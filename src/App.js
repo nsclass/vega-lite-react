6985f2b432106124b7d936f6e426cd04a8266999
Nam Seob Seo <nsclass@gmail.com>
2021-06-17 07:10:33 -0500

    vega lite api with remote data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useRef } from "react"
 import * as vl from "vega-lite-api"
 import * as vega from "vega"
 import * as vegaLite from "vega-lite"
+import { csv } from "d3"
 
 const data = [
   { a: "A", b: 28 },
@@ -16,18 +17,41 @@ const data = [
   { a: "I", b: 52 },
 ]
 
+async function getData(url) {
+  const data = await csv(url)
+  console.log(data[0])
+  return data
+}
+
+const dataUrl =
+  "https://gist.githubusercontent.com/curran/8c131a74b85d0bb0246233de2cff3f52/raw/194c2fc143790b937c42bf086a5a44cb3c55340e/auto-mpg.csv"
+
+const viz = vl
+  .markCircle({ size: 300, opacity: 0.5 })
+  .encode(
+    vl.x().fieldQ("mpg").scale({ zero: false }),
+    vl.y().fieldQ("horsepower").scale({ zero: false }),
+    vl.color().fieldN("origin"),
+    vl.size().fieldQ("weight"),
+    vl.tooltip().fieldN("name")
+  )
+
 const VegaLiteComponent = () => {
   const chartRef = useRef(null)
   useEffect(() => {
     vl.register(vega, vegaLite, {})
 
-    vl.markBar()
-      .data(data)
-      .encode(vl.x().fieldQ("b"), vl.y().fieldN("a"), vl.tooltip([vl.fieldQ("b"), vl.fieldN("a")]))
-      .render()
-      .then((chart) => {
-        chartRef.current.appendChild(chart)
-      })
+    getData(dataUrl).then((data) => {
+      viz
+        .data(data)
+        .width(window.innerWidth)
+        .height(window.innerHeight)
+        .autosize({ type: "fit", contains: "padding" })
+        .render()
+        .then((chart) => {
+          chartRef.current.appendChild(chart)
+        })
+    })
   }, [])
 
   return <div ref={chartRef}></div>
